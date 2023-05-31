4501e96c3716db6923ffbcac3b4a9154831e52ad
yuetsh <517252939@qq.com>
2023-05-31 20:27:40 +0800

    fix.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import legacy from "@vitejs/plugin-legacy"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), legacy({ targets: ["chrome 66", "not IE 11"] })],
 })
